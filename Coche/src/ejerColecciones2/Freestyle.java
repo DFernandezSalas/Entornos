f2d2f6f815fa0bd380c4e578d725b47501e07e8b
DFernandezSalas <0301dfernandez@e-itaca.es>
2024-05-13 13:33:31 +0200

    13/05/2024v1

--- Coche/src/ejerColecciones2/Freestyle.java
+++ Coche/src/ejerColecciones2/Freestyle.java
@@ -3,47 +3,65 @@ package ejerColecciones2;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
+import EjerColecciones.Producto;
 
 public class Freestyle {
-	private Map<String, ArrayList<Double>> jueces = new HashMap<>();
-	private Map<String, ArrayList<Double>> artistas = new HashMap<>();
+	private Map<String, Map<String, ArrayList<Double>>> jueces = new HashMap<>();
+	private Map<String, Map<String, ArrayList<Double>>> artistas = new HashMap<>();
 	private Map<String, Double> media = new HashMap<>();
-	private Map<String, Map<String, ArrayList<Double>>> mejorArtistaMetrica = new HashMap<>();
-	
+	private Map<String, Map<String, Map<String, ArrayList<Double>>>> mejorArtistaMetrica = new HashMap<>();
+
 	private String juez;
 	private String cantante;
 	private String metrica;
 	private double nota;
-	public Freestyle(String juez, String cantante, String metrica, double nota) {
-		//Map de los jueces y las notas que dan
-		if (jueces.containsKey(juez)) {
-			ArrayList<Double> j = jueces.get(juez);
-			j.add(nota);
-		} else {
-			ArrayList<Double> j = jueces.get(juez);
-			j.add(nota);
-			jueces.put(juez, j);
-		}
-		//Map de las notas que reciben los cantantes
-		if (artistas.containsKey(cantante)) {
-			ArrayList<Double> art = artistas.get(cantante);
-			art.add(nota);
-		} else {
-			ArrayList<Double> art = artistas.get(cantante);
-			art.add(nota);
-			artistas.put(juez, art);
-		}
-		//Map de las notas medias
-		if (media.containsKey(cantante)) {
-			ArrayList<Double> artist = artistas.get(cantante);
-			double media;
-			for (ArrayList<Double> n : artist) {
-				
-			}
-		}
-		
+
+	public void agregarNota(String juez, String cantante, String metrica, double nota) {
+		// Map de los jueces, las notas que dan y en que metrica
+//		if (jueces.containsKey(juez)) {
+//			Map<String, ArrayList<Double>> nombreJuez = jueces.get(juez);
+//			if (nombreJuez.containsKey(metrica)) {
+//				ArrayList<Double> nombreMetrica = nombreJuez.get(metrica);
+//				nombreMetrica.add(nota);
+//			} else {
+//				jueces.put(juez, metrica)
+//			}
+//		}
+		 if (!jueces.containsKey(juez)) {
+	            jueces.put(juez, new HashMap<>());
+	        }
+
+	        Map<String, ArrayList<Double>> metricasPorJuez = jueces.get(juez);
+
+	        ArrayList<Double> notas = metricasPorJuez.get(metrica);
+
+	        if (notas == null) {
+	            notas = new ArrayList<>();
+	            metricasPorJuez.put(metrica, notas);
+	        }
+
+	        notas.add(nota);
+
+		/*
+		 * if (jueces.containsKey(juez)) { ArrayList<Double> j = jueces.get(juez);
+		 * j.add(nota); } else { ArrayList<Double> j = jueces.get(juez); j.add(nota);
+		 * jueces.put(juez, j); }
+		 */
+		// Map de las notas que reciben los cantantes
+
 	}
-	
-	
-	
+	@Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, Map<String, ArrayList<Double>>> entry : jueces.entrySet()) {
+            sb.append("Juez: ").append(entry.getKey()).append("\n");
+            for (Map.Entry<String, ArrayList<Double>> innerEntry : entry.getValue().entrySet()) {
+                sb.append("\tMetrica: ").append(innerEntry.getKey()).append(", Notas: ").append(innerEntry.getValue()).append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
 }
---
+++ Coche/src/ejerColecciones2/Main.java
@@ -0,0 +1,14 @@
+package ejerColecciones2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Freestyle f1 = new Freestyle();
+		f1.agregarNota("Paco", "x", "rimas", 5);
+		f1.agregarNota("Paco", "x", "rimas", 7);
+		f1.agregarNota("Paco", "x", "ritmo", 7);
+		System.out.println(f1);
+	}
+
+}
